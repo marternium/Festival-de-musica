656c03af18e85dc76396f6b760d8b169ceb3dbe9
María Méndez <marternium@gmail.com>
2026-02-21 21:10:04 -0300

    ✨add gallery modal view

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,38 @@ function createGallery(){
         const image = document.createElement('img');
         image.src = `src/img/gallery/full/${i}.jpg`;
         image.alt = `Imagen de galería ${i}`;
+        image.onclick = () => {
+            showImage(i);
+        }
         gallery.appendChild(image);
     }
+}
+
+function showImage(i){
+    const image = document.createElement('img');
+    image.src = `src/img/gallery/full/${i}.jpg`;
+    image.alt = `Imagen de galería ${i}`;
+
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.onclick = closeModal;
+
+    const closeBtn = document.createElement('button');
+    closeBtn.textContent = 'X';
+    closeBtn.classList.add('btn-close');
+    closeBtn.onclick = closeModal;
+
+    modal.appendChild(image);
+    modal.appendChild(closeBtn);
+    document.body.classList.add('overflow-hidden');
+    document.body.appendChild(modal);
+}
+
+function closeModal(){
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out');
+    setTimeout(() => {
+        modal?.remove();
+        document.body.classList.remove('overflow-hidden');
+    }, 500);
 }
